7b19d6159b05838d8ba079c2da01b6a8e0c55a91
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-08 12:00:14 +0900

    초기 커밋.

---
+++ csloop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long start = DateTime.Now.Ticks;
+            long count = 0;
+
+            while (start + (10000000) > DateTime.Now.Ticks)
+            {
+                count++;
+            }
+            Console.WriteLine(count + "만큼 반복함");
+        }
+    }
+}
